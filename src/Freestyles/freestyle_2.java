4b1ca1b3964854fe23e087e9955246b68fff6083
Jazaltron <jazaltron10@gmail.com>
2021-07-01 15:12:15 +0100

    light changes

--- src/Freestyles/freestyle_2.java
+++ src/Freestyles/freestyle_2.java
@@ -116,7 +116,6 @@ public class freestyle_2 {
             return rezi;
         }
     }
-
     public static void main(String[] args) {
         Arrays();
         int[] myNums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
