05be50a0f877eea2bb5fbea565ad60808b4d8dce
Jazaltron <jazaltron10@gmail.com>
2021-07-05 16:23:57 +0100

    Modified a few files

--- src/CS/Quiz_2.java
+++ src/CS/Quiz_2.java
@@ -11,16 +11,11 @@ public class Quiz_2 {
             String answer = JOptionPane.showInputDialog(question);
             answer = answer.toUpperCase();
             if (answer.equals("A")) {
-                //JOptionPane.showMessageDialog(null, "Correct!");
                 return answer;
             } else if (answer.equals("B") || answer.equals("C") || answer.equals("D") || answer.equals("E")) {
-                //JOptionPane.showMessageDialog(null, "InCorrect!");
                 return answer;
-                // continue;
             } else {
                 JOptionPane.showMessageDialog(null, "Invalid answer please enter A, B, C, D, or E");
-                // return answer;
-                // continue;
             }
 
         }
@@ -45,35 +40,31 @@ public class Quiz_2 {
         question += "C. Germany\n";
         question += "D. Brazil\n";
         question += "E. Croatia\n";
-        System.out.println("\n" + question);
         check(question, "A");
 
         String question_2 = "Who won the 2016 Euro Cup\n";
-        question_2 += "A. Portugal\n";
+        question_2 += "A. Germany\n";
         question_2 += "B. Spain\n";
-        question_2 += "C. Germany\n";
+        question_2 += "C. Portugal\n";
         question_2 += "D. France\n";
         question_2 += "E. Italy\n";
-        System.out.println("\n" + question_2);
-        check(question_2, "A");
+        check(question_2, "C");
 
         String question_3 = "Who won the 2014 FIFA World Cup\n";
-        question_3 += "A. Germany\n";
+        question_3 += "A. Italy\n";
         question_3 += "B. Spain\n";
         question_3 += "C. Argentina\n";
         question_3 += "D. France\n";
-        question_3 += "E. Italy\n";
-        System.out.println("\n" + question_3);
-        check(question_3, "A");
+        question_3 += "E. Germany\n";
+        check(question_3, "E");
 
         String question_4 = "Who won the 2017 UEFA Champions League\n";
-        question_4 += "A. Real Madrid\n";
+        question_4 += "A. Barcelona\n";
         question_4 += "B. Arsenal\n";
         question_4 += "C. Norwich\n";
-        question_4 += "D. Burundi\n";
+        question_4 += "D. Real Madrid\n";
         question_4 += "E. Chelsea\n";
-        System.out.println("\n" + question_4);
-        check(question_4, "A");
+        check(question_4, "D");
 
         String question_5 = "Who won the 2020 English Premier League\n";
         question_5 += "A. Arsenal\n";
@@ -81,17 +72,9 @@ public class Quiz_2 {
         question_5 += "C. Leicester City\n";
         question_5 += "D. Manchester City\n";
         question_5 += "E. Chelsea\n";
-        System.out.println("\n" + question_5);
         check(question_5, "B");
 
         JOptionPane.showMessageDialog(null, nCorrect + " questions Correct out of " + nQuestions + " Questions");
 
-        // question = "Who won the 2015 UCL\n";
-        // question += "A. Barcelona\n";
-        // question += "B. Real Madrid\n";
-        // question += "C. Juventus\n";
-        // question += "D. Egypt\n";
-        // question += "E. Canada\n";
-        // check(question, "A");
     }
 }
--- src/DA_UNIT/Week_3.java
+++ src/DA_UNIT/Week_3.java
@@ -1,10 +1,20 @@
 package DA_UNIT;
-public class Week_3{
+
+public class Week_3 {
+    static void FormalParameters(int shake, String bake) {
+        System.out.println(bake + " is " + shake + " years old");
+    }
+
+    static void ActualParameters(String name, String car, String day) {
+        System.out.println(name + " loves to drive his " + car + " Only on a " + day);
+    }
+
     public static void main(String[] args) {
-        System.out.println("\nHello");
-        int countdown_f;
-        for (countdown_f = 3; countdown_f != 0; countdown_f--) {
-            System.out.println(countdown_f);
-        }
+        System.out.println("\nDA_Unit_3");
+        FormalParameters(26, "john");
+        String name = "Cristiano";
+        String car = "Bugatti";
+        String day = "Sunday";
+        ActualParameters(name, car, day);
     }
 }
