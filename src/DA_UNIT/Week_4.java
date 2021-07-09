cf3cb6ba5a0d046ac1d8712407d7f46f689221df
Jazaltron <jazaltron10@gmail.com>
2021-07-09 15:39:29 +0100

    OOP -> Week 4

--- src/DA_UNIT/Week_4.java
+++ src/DA_UNIT/Week_4.java
@@ -1,7 +1,24 @@
 package DA_UNIT;
 
+
+
 public class Week_4 {
-    public static void main(String[] args) {
-        System.out.println("Hello");
-    }
+    /**
+     * Objects are Entities that have behaviours, that hold information, and that
+     * can interact with one another.
+     * 
+     * objects can be thought of as things that have certain feature like how tall
+     * they are or how much they weigh and because they contain these distinct
+     * features they can act or behave in a certain way, with regards to themselves
+     * and other objects.
+     * 
+     * in java variables do not store or hold objects, but rather they store
+     * references or pointers to where the objects are stored in memory.
+     * 
+     * When one object variable is assigned to another, only a reference is copied.
+     * The object referred to is not copied.
+     */
+
+    
+        
 }
--- src/Freestyles/freestyle_4.java
+++ src/Freestyles/freestyle_4.java
@@ -1,6 +1,12 @@
 package Freestyles;
 public class Freestyle_4 {
     public static void main(String[] args) {
-
+        String x = "james";
+        int y = 22;
+        StringBuilder builder = new StringBuilder();
+        builder.append(x);
+        builder.append(y);
+        System.out.println(builder);
+        System.out.println(builder.toString());
     }
 }
