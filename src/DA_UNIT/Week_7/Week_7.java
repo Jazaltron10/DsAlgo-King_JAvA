0517007e5ea587546b2e9541414faa00d5ea9035
Jazaltron <jazaltron10@gmail.com>
2021-07-29 10:48:10 +0100

    New week7 files and data structure files

---
+++ src/DA_UNIT/Week_7/Week_7.java
@@ -0,0 +1,8 @@
+package DA_UNIT.Week_7;
+
+public class Week_7 {
+    public static void main(String[] args) {
+        String newline = "\n";
+        System.out.println(newline + "Is This Java For Real");
+    }
+}
---
+++ src/DataStructures/ArrayLists/ArrayLists.java
@@ -0,0 +1,7 @@
+package DataStructures.ArrayLists;
+
+public class ArrayLists {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
