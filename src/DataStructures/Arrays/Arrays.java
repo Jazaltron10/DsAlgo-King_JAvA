1b1830a14d1be8e76ab002798a7945fff58ea4bc
Jazaltron <jazaltron10@gmail.com>
2021-08-01 19:26:01 +0100

    Still Making little Modifications to Array.java

--- src/DataStructures/Arrays/Arrays.java
+++ src/DataStructures/Arrays/Arrays.java
@@ -1,28 +1,34 @@
 package DataStructures.Arrays;
 
 public class Arrays {
+    static String nLine = "\n";
     static void arrayRepublic_1() {
 
         String lines[]; // declaring the array
         lines = new String[] { "John", "James", "James", "Baldwin", "Johnson" };// Initializing the array
         
         boolean dupp = false; // Assume there are no duplicates
-        for (int i = 0; i < lines.length - 1; i++) {
+        for (int i = 0; i < lines.length - 1 ; i++ ) {
             if (lines[i].equals(lines[i + 1])) {
                 dupp = true;
-                System.out.println("This Array contains duplicates");
-                break;
-            } else {
-                System.out.println("This Array contains no duplicates");
+                System.out.println("This Array contains duplicates"+ nLine);
                 break;
             }
         }
     }
+        // else {
+        //     System.out.println("This Array contains no duplicates");
+        //     break;
+        // }
     
     public static void main(String[] args) {
+        arrayRepublic_1();
+        
         System.out.println("Hello World\n");
+        
+        
         String lines[]; // declaring the array
-        lines = new String[] { "J", "J", "J", "B", "J" };// Initializing the array
+        lines = new String[] { "John", "James", "James", "B", "J" };// Initializing the array
         boolean dupp = false; // Assume there are no duplicates
         for (int i = 0; i < lines.length - 1; i++) {
             if (lines[i].equals(lines[i + 1])) {
