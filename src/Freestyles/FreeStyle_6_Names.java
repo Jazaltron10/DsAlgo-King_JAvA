3ecfca724d44a7da49753e8dcb947bd9996cc05c
Jazaltron <jazaltron10@gmail.com>
2021-08-04 08:59:07 +0100

    Names Modified

--- src/Freestyles/FreeStyle_6_Names.java
+++ src/Freestyles/FreeStyle_6_Names.java
@@ -1,9 +1,12 @@
 package Freestyles;
-
+import java.util.*;
 public class FreeStyle_6_Names {
     public static void main(String[] args) {
         for (int i = 0; i > 10; i++){
             System.out.println("Hello");
         }
+
+        Integer[] a = new Integer[10];
+        System.out.println(Arrays.toString(a));
     }
 }
