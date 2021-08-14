776de598bef4673092e2194098f782e412d7a6b6
Jazaltron <jazaltron10@gmail.com>
2021-08-14 08:34:56 +0100

    Modified Week_5.java

--- src/DA_UNIT/Week_5/Week_5.java
+++ src/DA_UNIT/Week_5/Week_5.java
@@ -1,33 +1,36 @@
 package DA_UNIT.Week_5;
 
+import java.util.*;
+
 public class Week_5 {
         public static void main(String[] args) {
                 // Section for the Base Person Class:
                 // name , age
-                Person Africa = new Person("Cyprian Ekwensi", 60);
-                Africa.introduction();
-                Africa.setFeatures("Chinua Achebe", 26, 1.87, 82, "male", "Nigeria");
-                Africa.introduction();
-                Africa.getinfo();
+                Person African = new Person("Cyprian Ekwensi", 60);
+                African.introduction();
+                African.setFeatures("Chinua Achebe", 26, 1.87, 82, "male", "Nigeria");
+                African.introduction();
+                African.getinfo();
 
                 // String name, int age, double height, int weight, String gender, String
                 // country
-                Person Europe = new Person("Jack Grealish", 30, 1.83, 79, "male", "England");
-                Europe.getinfo();
+                Person European = new Person("Jack Grealish", 30, 1.83, 79, "male", "England");
+                European.getinfo();
 
-                Person Asia = new Person("Uzumaki Himawari", 22, 1.58, 63, "female", "Japan");
-                Asia.getinfo();
+                Person Asian = new Person("Uzumaki Himawari", 22, 1.58, 63, "female", "Japan");
+                Asian.getinfo();
 
-                Person SouthAmerica = new Person("Isabela Gomez", 19, 1.62, 70, "female", "Brazil");
-                SouthAmerica.getinfo();
+                Person SouthAmerican = new Person("Isabela Gomez", 19, 1.62, 70, "female", "Brazil");
+                SouthAmerican.getinfo();
 
-                Person NorthAmerica = new Person("Bruce Wayne", 39, 1.66, 80, "male", "America");
-                NorthAmerica.getinfo();
+                Person NorthAmerican = new Person("Bruce Wayne", 39, 1.66, 80, "male", "America");
+                NorthAmerican.getinfo();
 
                 // Section For Doctor Class:
                 // name, age, medicalField, employer, qualification, residency,
                 // YearsOfExperience
-                Doctor Surgeon = new Doctor("female", "Arabella Torres", 45, "Surgery", "Hospital", "M.D. and MCAT", true, 15);
+                Doctor Surgeon = new Doctor("female", "Arabella Torres", 45, "Surgery", "Hospital", "M.D. and MCAT",
+                                true, 15);
                 Surgeon.getinfo();
 
                 Doctor Cardiologist = new Doctor("male", "Dernis Boyle", 35, "Cardiology", "Armed Services",
@@ -38,40 +41,54 @@ public class Week_5 {
                                 "DAT, DDS and DMD", false, 7);
                 Dentist.getinfo();
 
-                Doctor Podiatrist = new Doctor("female", "Nalini Vishwakumar", 29, "Dermatology", "Community Health Clinic",
-                                "MCAT, GRE and DPM", true, 9);
+                Doctor Podiatrist = new Doctor("female", "Nalini Vishwakumar", 29, "Dermatology",
+                                "Community Health Clinic", "MCAT, GRE and DPM", true, 9);
                 Podiatrist.getinfo();
 
                 Doctor Pharmacist = new Doctor("female", "Rainbow Johnson", 36, "Psychopharmacotherapy",
                                 "Pharmaceutical company", "PCAT and Pharm.D.", false, 11);
                 Pharmacist.getinfo();
 
+                
                 // Section for the SoftwareEngineer Class
                 // String gender, String name, int age, String specialization, String role,
                 // String company, String salary, String level
                 SoftwareEngineer WebDeveloper = new SoftwareEngineer("female", "Maya Kricko", 27, "Web DevelopMent",
                                 "Front-End Engineer", "Google", "$658,962", "L7", 10);
-                WebDeveloper.getinfo();
-                WebDeveloper.Value();
 
                 Person MobileDeveloper = new SoftwareEngineer();
                 ((SoftwareEngineer) MobileDeveloper).setFeatures("male", "Cristiano Ronaldo", 32, "Apple IOS",
                                 "Mobile Developer", "Apple", "$258,962", "ICT 3", 5);
-                ((SoftwareEngineer) MobileDeveloper).Bio();
-                ((SoftwareEngineer) MobileDeveloper).getinfo();
-                ((SoftwareEngineer) MobileDeveloper).Value();
 
                 SoftwareEngineer CloudEngineer = new SoftwareEngineer("male", "Mike Ross", 45, "Azure Cloud",
                                 "Cloud Architect", "Microsoft", "$872,650", "Partner", 15);
-                CloudEngineer.getinfo();
 
-                SoftwareEngineer TestingEngineer = new SoftwareEngineer("female", "Jasmine Neutron", 37, "Software Testing",
-                                "QA Engineer", "Facebook", "$396,802", "E5", 7);
-                TestingEngineer.getinfo();
+                SoftwareEngineer TestingEngineer = new SoftwareEngineer("female", "Jasmine Neutron", 37,
+                                "Software Testing", "QA Engineer", "Facebook", "$396,802", "E5", 7);
 
                 SoftwareEngineer AutomationEngineer = new SoftwareEngineer("male", "Jake Sully", 29, "AWS Cloud",
                                 "DevOps Engineer", "Netflix", "$506,788", "Senior Software Engineer", 10);
-                AutomationEngineer.getinfo();
-                AutomationEngineer.testingsuper();
+
+                // Using An ArrayList to Manipulate all created Objects
+                ArrayList<Person> SWE = new ArrayList<Person>();
+                SWE.add(WebDeveloper);
+                SWE.add(MobileDeveloper);
+                SWE.add(CloudEngineer);
+                SWE.add(TestingEngineer);
+                SWE.add(AutomationEngineer);
+                int count = 0;
+                String tab = "\t";
+                for (Person item : SWE) {
+                        count++;
+                        System.out.printf("\n%s%s%s\n", tab.repeat(10), "Engineer ", count);
+                        ((SoftwareEngineer) item).Bio();
+                        item.getinfo();
+                        if (count == 1 || count == 2) {
+                                ((SoftwareEngineer) item).Value();
+                        }
+                        if (count == 5) {
+                                ((SoftwareEngineer) item).testingsuper();
+                        }
+                }
         }
 }
--- src/Prep/Prep.java
+++
@@ -1,7 +0,0 @@
-package Prep;
-public class Prep {
-    
-}
-/**
- * I feel the same way about moving back to Jamaica from the UK 4 years ago. Probably my biggest mistake to date. Everything mentioned in the video is exactly the same here and more. The nepotism, the profiling and keeping up appearances while dying daily, the scamming, the lack of security especially for women, the need to be aggressive and a bully to get anywhere or get anything done, the lackadaisical approach to everything, the daily reinvention of the wheel, the working to death to barely survive, the garbage everywhere, the exorbitant inflation rate and the crap salaries, the hustle mentality (and not in a good healthy way). It seems like this is the definition of third world living.
-*/
