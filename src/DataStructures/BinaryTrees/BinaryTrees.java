96e26569ca4823f452603b790d39f1d156518f22
Jazaltron <jazaltron10@gmail.com>
2021-08-31 17:41:18 +0100

    Working with binary trees

--- src/DataStructures/BinaryTrees/BinaryTrees.java
+++
@@ -1,8 +0,0 @@
-import java.io.*;
-import java.util.*;
-public class BinaryTrees {
-	public static void main(String args[]){
-        System.out.println("Hello");
-	} // END MAIN
-} // CLASS BINARY TREES    
-
--- src/DataStructures/BinaryTrees/Node.java
+++ src/DataStructures/BinaryTrees/Node.java
@@ -2,4 +2,9 @@ public class Node {
     int data; // This is the data in this node
     Node left; // pointer to the left subtree
     Node right; // pointer to the right subtree
+
+    public Node(int data){
+        this.data = data;
+        left = right = null;
+    }
 }
