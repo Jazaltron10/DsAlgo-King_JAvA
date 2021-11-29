2b2ae5452c9fbc9a9d9cc00ec73b6189bce0a994
Jazaltron <jazaltron10@gmail.com>
2021-11-29 21:46:28 +0100

    Evening stroll with code

--- src/CodingQuestions/Sort_2.java
+++ src/CodingQuestions/Sort_2.java
@@ -1,5 +1,18 @@
 import java.util.*;
 public class Sort_2 {
+    public static int commonChild(String s1, String s2){
+        int[][] dp = new int[s1.length()+1][s2.length()+1];
+        for(int i=1;i<=s1.length();i++){
+            for(int j=1;j<=s2.length();j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1]+1;
+                }else{
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[s1.length()][s2.length()];
+    }
     public static void main(String[] args) {
         String[] k = new String[] { "1,44","3,4","5,63","5,6","3,2","4,3"};
         System.out.println(Arrays.toString(k));
--- src/CodingQuestions/Stringas.java
+++ src/CodingQuestions/Stringas.java
@@ -1,5 +1,59 @@
+import java.util.*;
+
 public class Stringas {
-    public static void main(String[] args) {
-        
+    // Java implementation of the approach
+
+    static int commonChild(String s1, String s2) {
+        return LCSM4(s1.toCharArray(), s2.toCharArray(), s1.length(), s2.length());
+    }
+
+    public static int LCSM4(char[] X, char[] Y, int m, int n) {
+        int memo[] = new int[n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            int prev = 0;
+            for (int j = 1; j <= n; j++) {
+                int temp = memo[j];
+                if (X[i - 1] == Y[j - 1]) {
+                    memo[j] = prev + 1;
+                } else {
+                    memo[j] = Math.max(memo[j], memo[j - 1]);
+                }
+                prev = temp;
+            }
+        }
+        System.out.println(Arrays.toString(memo));
+        return memo[n];
+    }
+
+    // Driver code
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.next();
+        String s2 = sc.next();
+        int result = commonChild(s1, s2);
+        System.out.println(result);
+        sc.close();
+
+
+        // String s1 = "SHINCHAN";
+        // char [] cal = s1.toCharArray();
+        // System.out.println(cal[0]);
+        // System.out.println(0 + 'a');
+        // String bigletters = "";
+        // String small_letters = "";
+
+        // for (char c = 65; c <= 90; c++) {
+        //     bigletters += c + " ";
+        // }
+        // for (char d = 97; d <= 'z'; d++) {
+        //     small_letters += d + " ";
+        // }
+
+        // System.out.println(bigletters + "\n");
+        // System.out.println(small_letters);
+        // System.out.println(Math.max(20, 3));
+        // System.out.println(Math.min(20, 3));
+        // System.out.println((int) Math.floor(Math.random() * 3));
     }
 }
